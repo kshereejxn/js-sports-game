52b7bc19c7e81cf12f971c8ff5a170894070caa0
Trinie J <jxsn00ks@gmail.com>
2020-04-30 03:14:24 -0400

    having trouble with evenlistener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,12 @@
-/*random chance is
-Math.floor(Math.random() * 3);
-*/
+let teamone_numshots = document.querySelector ("#t1shots");
+let teamone_numgoals = document.querySelector ("#t1goals");
+let teamone_shoot_button = document.querySelector ("#t1shootbutton");
+let teamtwo_numshots = document.querySelector ("#t2shots");
+let teamtwo_numgoals = document.querySelector ("#t2goals");
+let teamtwo_shoot_button = document.querySelector ("#t2shootbutton");
+let reset_button = document.querySelector ("#resetbutton");
+let num_resets = document.querySelector ("#numresets");
 
-let count = 0;
-
-
-/*let count = 0;
-const countspan = document.querySelector("#countspan");
-const counter_button = document.querySelector("#counter_button");
-counter_button.addEventListener("click", function () { 
-    console.log (" count")
-    count = count + 1;
-    countspan.innerHTML = count
-
+teamone_shoot_button.addEventListener("click", function(){
+    console.log ("test")
 })
-const even_or_odd_button = document.querySelector("#even_or_odd_button");
-even_or_odd_button.addEventListener("click", function () {
-    if (count %2===0)
-    console.log ("even")
-    else
-    console.log ("odd")
-   
-})
-*/
