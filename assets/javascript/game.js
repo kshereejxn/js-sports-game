d210379b510b09327de260af29b3a500b180fcd4
Trinie J <jxsn00ks@gmail.com>
2020-04-29 22:32:42 -0400

    correct commit of file change

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+/*random chance is
+Math.floor(Math.random() * 3);
+*/
+
+let count = 0;
+
+
+/*let count = 0;
+const countspan = document.querySelector("#countspan");
+const counter_button = document.querySelector("#counter_button");
+counter_button.addEventListener("click", function () { 
+    console.log (" count")
+    count = count + 1;
+    countspan.innerHTML = count
+
+})
+const even_or_odd_button = document.querySelector("#even_or_odd_button");
+even_or_odd_button.addEventListener("click", function () {
+    if (count %2===0)
+    console.log ("even")
+    else
+    console.log ("odd")
+   
+})
+*/
