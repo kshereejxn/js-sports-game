d0d9814d6e5d47a8d069512167b1c50c6bc62c66
Trinie J <jxsn00ks@gmail.com>
2020-05-04 02:57:44 -0400

    shots formula updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,20 @@ let teamtwo_shoot_button = document.querySelector ("#teamtwo-shoot-button");
 let reset_button = document.querySelector ("#reset-button");
 let num_resets = document.querySelector ("#num-resets");
 
-teamone_shoot_button.addEventListener("click", function(){
-    console.log ("shot for team one")
+teamone_shoot_button.addEventListener("click",      function(){
+    console.log (" shots for team one")
     
    document.getElementById("teamone-shoot-button").innerHTML=Math.floor(Math.random() * 4);
+ 
+let newteamone_numshotsValue = parseInt(teamone_numshots.innerHTML) + 1;
+
+teamone_numshots.innerHTML = newteamone_numshotsValue;
+
+
+
+let newteamone_numgoalsValue = parseInt (newteamone_numshotsValue) ;
+
+teamone_numgoals.innerHTML = newteamone_numgoalsValue;
 
 })
 
