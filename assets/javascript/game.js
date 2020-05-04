d4f74c5408e541be2d920548f8b62dab7d0b9bed
Trinie J <jxsn00ks@gmail.com>
2020-05-04 00:39:47 -0400

    found math formula

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,22 @@
-let teamone_numshots = document.querySelector ("#t1shots");
-let teamone_numgoals = document.querySelector ("#t1goals");
-let teamone_shoot_button = document.querySelector ("#t1shootbutton");
-let teamtwo_numshots = document.querySelector ("#t2shots");
-let teamtwo_numgoals = document.querySelector ("#t2goals");
-let teamtwo_shoot_button = document.querySelector ("#t2shootbutton");
-let reset_button = document.querySelector ("#resetbutton");
-let num_resets = document.querySelector ("#numresets");
+let teamone_numshots = document.querySelector ("#teamone-numshots");
+let teamone_numgoals = document.querySelector ("#teamone-numgoals");
+let teamone_shoot_button = document.querySelector ("#teamone-shoot-button");
+let teamtwo_numshots = document.querySelector ("#teamtwo-numshots");
+let teamtwo_numgoals = document.querySelector ("#teamtwo-numgoals");
+let teamtwo_shoot_button = document.querySelector ("#teamtwo-shoot-button");
+let reset_button = document.querySelector ("#reset-button");
+let num_resets = document.querySelector ("#num-resets");
 
 teamone_shoot_button.addEventListener("click", function(){
-    console.log ("test")
+    console.log ("shot for team one")
+    
+   document.getElementById("teamone-shoot-button").innerHTML=Math.floor(Math.random() * 4);
+
+})
+
+teamtwo_shoot_button.addEventListener("click", function(){
+    console.log ("shot for team two")
+    
+   document.getElementById("teamtwo-shoot-button").innerHTML=Math.floor(Math.random() * 4);
+
 })
