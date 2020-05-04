bc1d9f15d3db00a967172edc938e6a8b9f8aa7ab
Trinie J <jxsn00ks@gmail.com>
2020-05-04 11:01:54 -0400

    goal formula updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,27 +6,30 @@ let teamtwo_numgoals = document.querySelector ("#teamtwo-numgoals");
 let teamtwo_shoot_button = document.querySelector ("#teamtwo-shoot-button");
 let reset_button = document.querySelector ("#reset-button");
 let num_resets = document.querySelector ("#num-resets");
+let count = 1;
+
 
 teamone_shoot_button.addEventListener("click",      function(){
     console.log (" shots for team one")
     
-   document.getElementById("teamone-shoot-button").innerHTML=Math.floor(Math.random() * 4);
- 
-let newteamone_numshotsValue = parseInt(teamone_numshots.innerHTML) + 1;
+   let newTeamone_numshotsValue = Number(teamone_numshots.innerHTML) + 1;
 
-teamone_numshots.innerHTML = newteamone_numshotsValue;
+   teamone_numshots.innerHTML = newTeamone_numshotsValue;
+   
+  document.getElementById("teamone-numshots").innerHTML=Math.floor(Math.random() * 4);
 
 
-
-let newteamone_numgoalsValue = parseInt (newteamone_numshotsValue) ;
+let newteamone_numgoalsValue = Number (teamone_numgoals.innerHTML);
 
 teamone_numgoals.innerHTML = newteamone_numgoalsValue;
 
+if (teamone_numshots.innerHTML > 0) {teamone_numgoals.innerHTML = newteamone_numgoalsValue + 1;
+}
 })
 
-teamtwo_shoot_button.addEventListener("click", function(){
-    console.log ("shot for team two")
-    
-   document.getElementById("teamtwo-shoot-button").innerHTML=Math.floor(Math.random() * 4);
 
-})
+/*reset_button.addEventListener("click", 
+function() {
+console.log (" resets have occurred");
+reset_button.innerHTML = + 1;
+})*/
